61da6cd227caef55cf01ee3f02a39c80600e2b3b
Trung Kien <lykien172@gmail.com>
2026-03-06 10:43:57 +0700

    fix: update vite config for public directory 4

--- Frontend/VirtualTravel/vite.config.js
+++ Frontend/VirtualTravel/vite.config.js
@@ -3,20 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  publicDir: "public",
 
   server: {
     port: 5173,
     open: true,
-    strictPort: true,
-    cors: true,
   },
 
   preview: {
     port: 4173,
-    strictPort: true,
-  },
-
-  build: {
-    assetsInlineLimit: 0
   }
 });
