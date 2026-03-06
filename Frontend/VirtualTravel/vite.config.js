718f70b362bcdea83f7def4be51b63b60ab9f471
Trung Kien <lykien172@gmail.com>
2026-03-06 09:46:32 +0700

    fix: update vite config for public directory 1

--- Frontend/VirtualTravel/vite.config.js
+++ Frontend/VirtualTravel/vite.config.js
@@ -1,30 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Thêm dòng này để xác định rõ thư mục chứa file tĩnh là public
-  publicDir: 'public', 
+
   server: {
     port: 5173,
     open: true,
     strictPort: true,
     cors: true,
   },
+
   preview: {
     port: 4173,
     strictPort: true,
   },
-  // Thêm phần này để đảm bảo Vite không cố gắng xử lý các file trong thư mục js và css
+
   build: {
-    assetsInlineLimit: 0, 
-    rollupOptions: {
-      external: [
-        /^js\/.*/,
-        /^css\/.*/,
-        /^images\/.*/
-      ]
-    }
+    assetsInlineLimit: 0
   }
 });
